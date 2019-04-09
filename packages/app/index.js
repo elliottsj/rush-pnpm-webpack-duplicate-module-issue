e1c9b08ac9394c753987f4b534b33015864fd462
Spencer Elliott <me@elliottsj.com>
2019-04-09 15:03:53 -0400

    Repro

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,4 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(React.createElement('div', {}, 'Hi'), document.getElementById('root'));
+import styled from '@emotion/styled';
+import { ThemeProvider } from 'emotion-theming';
+
+const theme = {
+  primaryColor: 'blue',
+};
+
+const StyledDiv = styled.div(props => ({
+  backgroundColor: props.theme.primaryColor || 'red'
+}));
+
+// `
+//   background-color: ${props => props.theme.primaryColor}
+// `;
+
+const App = () => (
+  React.createElement(
+    ThemeProvider,
+    { theme: theme },
+    React.createElement(StyledDiv, {}, 'Bye')
+  )
+);
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
