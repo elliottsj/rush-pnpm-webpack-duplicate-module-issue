f4936556e0ad29b503f4cf96ac4433221fbb7ded
Spencer Elliott <me@elliottsj.com>
2019-04-09 14:49:24 -0400

    First commit

---
+++ packages/app/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(React.createElement('div', {}, 'Hi'), document.getElementById('root'));
---
+++ packages/component-lib/index.js
@@ -0,0 +1,1 @@
+module.exports = require('@smooth-ui/core-em');
